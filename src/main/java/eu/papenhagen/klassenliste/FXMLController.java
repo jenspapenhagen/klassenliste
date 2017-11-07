b595fcd72a372e1818437e100bb492c8fe76f2a5
jenspapenhagen <jens.papenhagen@web.de>
2017-11-07 20:34:02 +0100

    adding contextmenu

--- src/main/java/eu/papenhagen/klassenliste/FXMLController.java
+++ src/main/java/eu/papenhagen/klassenliste/FXMLController.java
@@ -8,9 +8,12 @@ import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -22,7 +25,7 @@ public class FXMLController implements Initializable {
 
     @FXML
     private TableView table;
-    
+
     @FXML
     private TableColumn name;
     @FXML
@@ -41,24 +44,37 @@ public class FXMLController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
         EntityManager em = new EntityManager();
         List<Member> memberlist = new ArrayList<>();
-        
         memberlist = em.getDate();
-        System.out.println(""+memberlist.get(0).getNachname());
+
+        ObservableList<Member> data = FXCollections.observableArrayList(memberlist);
+
+        //fill the table
+        name.setCellValueFactory(new PropertyValueFactory<Member, String>("name"));
+        nachname.setCellValueFactory(new PropertyValueFactory<Member, String>("nachname"));
+        gender.setCellValueFactory(new PropertyValueFactory<Member, Boolean>("gender"));
+        age.setCellValueFactory(new PropertyValueFactory<Member, Integer>("age"));
+        bemerkung.setCellValueFactory(new PropertyValueFactory<Member, String>("bemerkung"));
+
+        //the data
+        table.setItems(data);
+
+        // Create ContextMenu
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem edit = new MenuItem("Edit Member");
+        MenuItem delete = new MenuItem("Create Member");
         
-        //ObservableList<Member> list = FXCollections.observableArrayList(memberlist);
-        
-//        //fill the table
-//        name.setCellValueFactory(new PropertyValueFactory<Member,String>("name"));
-//        nachname.setCellValueFactory(new PropertyValueFactory<Member,String>("nachname"));
-//        gender.setCellValueFactory(new PropertyValueFactory<Member,Boolean>("gender"));
-//        age.setCellValueFactory(new PropertyValueFactory<Member,Integer>("age"));
-//        bemerkung.setCellValueFactory(new PropertyValueFactory<Member,String>("bemerkung"));
-        
-        
-        
-        
+        edit.setOnAction((ActionEvent event) -> {
+            label.setText("Select Menu Item 1");
+        });
+        delete.setOnAction((ActionEvent event) -> {
+            label.setText("Select Menu Item 2");
+        });
+
+        // Add MenuItem to ContextMenu
+        contextMenu.getItems().addAll(edit, delete);
+
     }
 }
